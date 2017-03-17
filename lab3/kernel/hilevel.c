b632894f18705eaa68ae4705de3ea3bdd1eeac8f
Jacob Montgomery <jm15969@bristol.ac.uk>
2017-03-17 17:46:06 +0000

    hilevel.c lab3

--- lab3/kernel/hilevel.c
+++ lab3/kernel/hilevel.c
@@ -1,8 +1,8 @@
 #include "hilevel.h"
 
-/* Since we *know* there will be 2 processes, stemming from the 2 user 
- * programs, we can 
- * 
+/* Since we *know* there will be 2 processes, stemming from the 2 user
+ * programs, we can
+ *
  * - allocate a fixed-size process table (of PCBs), and use a pointer
  *   to keep track of which entry is currently executing, and
  * - employ a fixed-case of round-robin scheduling: no more processes
@@ -15,7 +15,7 @@ void scheduler( ctx_t* ctx ) {
   if      ( current == &pcb[ 0 ] ) {
     memcpy( &pcb[ 0 ].ctx, ctx, sizeof( ctx_t ) ); // preserve P_1
     memcpy( ctx, &pcb[ 1 ].ctx, sizeof( ctx_t ) ); // restore  P_2
-    current = &pcb[ 1 ];                           
+    current = &pcb[ 1 ];
   }
   else if ( current == &pcb[ 1 ] ) {
     memcpy( &pcb[ 1 ].ctx, ctx, sizeof( ctx_t ) ); // preserve P_2
@@ -26,18 +26,36 @@ void scheduler( ctx_t* ctx ) {
   return;
 }
 
-extern void     main_P1(); 
+extern void     main_P1();
 extern uint32_t tos_P1;
-extern void     main_P2(); 
+extern void     main_P2();
 extern uint32_t tos_P2;
 
-void hilevel_handler_rst( ctx_t* ctx              ) { 
+void hilevel_handler_irq() {
+  // Step 2: read  the interrupt identifier so we know the source.
+
+  uint32_t id = GICC0->IAR;
+
+  // Step 4: handle the interrupt, then clear (or reset) the source.
+
+  if( id == GIC_SOURCE_TIMER0 ) {
+    PL011_putc( UART0, 'T', true ); TIMER0->Timer1IntClr = 0x01;
+  }
+
+  // Step 5: write the interrupt identifier to signal we're done.
+
+  GICC0->EOIR = id;
+
+  return;
+}
+
+void hilevel_handler_rst( ctx_t* ctx              ) {
   /* Initialise PCBs representing processes stemming from execution of
    * the two user programs.  Note in each case that
-   *    
-   * - the CPSR value of 0x50 means the processor is switched into USR 
+   *
+   * - the CPSR value of 0x50 means the processor is switched into USR
    *   mode, with IRQ interrupts enabled, and
-   * - the PC and SP values matche the entry point and top of stack. 
+   * - the PC and SP values matche the entry point and top of stack.
    */
 
   memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );
@@ -61,9 +79,9 @@ void hilevel_handler_rst( ctx_t* ctx              ) {
   return;
 }
 
-void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) { 
+void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
   /* Based on the identified encoded as an immediate operand in the
-   * instruction, 
+   * instruction,
    *
    * - read  the arguments from preserved usr mode registers,
    * - perform whatever is appropriate for this system call,
@@ -76,14 +94,14 @@ void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
       break;
     }
     case 0x01 : { // 0x01 => write( fd, x, n )
-      int   fd = ( int   )( ctx->gpr[ 0 ] );  
-      char*  x = ( char* )( ctx->gpr[ 1 ] );  
-      int    n = ( int   )( ctx->gpr[ 2 ] ); 
+      int   fd = ( int   )( ctx->gpr[ 0 ] );
+      char*  x = ( char* )( ctx->gpr[ 1 ] );
+      int    n = ( int   )( ctx->gpr[ 2 ] );
 
       for( int i = 0; i < n; i++ ) {
         PL011_putc( UART0, *x++, true );
       }
-      
+
       ctx->gpr[ 0 ] = n;
       break;
     }
