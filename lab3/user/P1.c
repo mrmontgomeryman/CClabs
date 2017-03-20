2d289f68cb1ca0dd0f3dcc59beeba8016942caa4
Jacob Montgomery <jm15969@bristol.ac.uk>
2017-03-20 17:12:36 +0000

    hilevel.c lab3

--- lab3/user/P1.c
+++ lab3/user/P1.c
@@ -2,7 +2,7 @@
 
 void main_P1() {
   while( 1 ) {
-    write( STDOUT_FILENO, "P1", 2 ); yield();
+    write( STDOUT_FILENO, "P1", 2 ); 
   }
 
   exit( EXIT_SUCCESS );
--- lab3/user/P2.c
+++ lab3/user/P2.c
@@ -2,7 +2,7 @@
 
 void main_P2() {
   while( 1 ) {
-    write( STDOUT_FILENO, "P2", 2 ); yield();
+    write( STDOUT_FILENO, "P2", 2 ); 
   }
 
   exit( EXIT_SUCCESS );
